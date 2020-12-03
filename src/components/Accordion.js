67f5c80866f5f23e38eb50d48421042b7894245e
Brianabeno <brianabeno@gmail.com>
2020-12-03 07:10:31 -0600

    Final Code

---
+++ src/components/Accordion.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Data } from './Data';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+const AccordionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  background: #fff;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 30%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+
+const Wrap = styled.div`
+  background: #272727;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+
+  h1 {
+    padding: 2rem;
+    font-size: 2rem;
+  }
+
+  span {
+    margin-right: 1.5rem;
+  }
+`;
+
+const Dropdown = styled.div`
+  background: #1c1c1c;
+  color: #00ffb9;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #00ffb9;
+  border-top: 1px solid #00ffb9;
+
+  p {
+    font-size: 2rem;
+  }
+`;
+
+const Accordion = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = index => {
+    if (clicked === index) {
+      //if clicked question is already active, then close it
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
+  return (
+    <IconContext.Provider value={{ color: '#00FFB9', size: '25px' }}>
+      <AccordionSection>
+        <Container>
+          {Data.map((item, index) => {
+            return (
+              <>
+                <Wrap onClick={() => toggle(index)} key={index}>
+                  <h1>{item.question}</h1>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+                </Wrap>
+                {clicked === index ? (
+                  <Dropdown>
+                    <p>{item.answer}</p>
+                  </Dropdown>
+                ) : null}
+              </>
+            );
+          })}
+        </Container>
+      </AccordionSection>
+    </IconContext.Provider>
+  );
+};
+
+export default Accordion;
---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    question: 'What do you call a dog magician?',
+    answer: 'A labracadabrador.'
+  },
+  {
+    question: 'What do you call a funny mountain?',
+    answer: 'Hill-arious.'
+  },
+  {
+    question: 'What did the astronaut say when he crashed into the moon?',
+    answer: 'I Apollo-gize.'
+  }
+];
